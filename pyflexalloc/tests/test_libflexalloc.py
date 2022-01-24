4046ee4542171202b10985f4a77cd47ea5159904
Jesper Wendel Devantier <j.devantier@samsung.com>
2022-01-24 13:23:40 +0100

    variant test: (re-)open pool, then read object
    Signed-off-by: Jesper Wendel Devantier <j.devantier@samsung.com>

--- pyflexalloc/tests/test_libflexalloc.py
+++ pyflexalloc/tests/test_libflexalloc.py
@@ -50,3 +50,48 @@ def test_loop_write_read(fs):
             print("free buf2")
 
         print("close fs")
+
+
+@pytest.mark.parametrize("fs", [
+    fla_open_direct(
+        device=dev_loop(size_mb=100, block_size_bytes=512),
+        formatter=format_fla_mkfs(npools=10, slab_nlb=1000)
+    ),
+    fla_open_daemon(
+        device=dev_loop(size_mb=100, block_size_bytes=512),
+        formatter=format_fla_mkfs(npools=10, slab_nlb=1000)
+    )
+])
+def test_loop_write_read_open_pool(fs):
+    with fs() as fs:
+        print("creating pool")
+        p1 = libflexalloc.pool_create(fs, "lol", 10)
+        print("object_alloc")
+        p1o1 = libflexalloc.object_create(fs, p1)
+        print(p1o1)
+        print("IOBuffer (#1)")
+
+        msg = "hello"
+
+        with flexalloc.io_buffer(fs, 512 * 8) as buf:
+            print("write to memoryview")
+            buf.view[0:5] = msg.encode("ascii")
+            print("object_write")
+            libflexalloc.object_write(fs, p1, p1o1, buf, 0, 512 * 8)
+
+        print("open pool again")
+        p1 = libflexalloc.pool_open(fs, "lol")
+        try:
+            with flexalloc.io_buffer(fs, 512 * 12) as buf2:
+                print("object_read")
+                libflexalloc.object_read(fs, p1, p1o1, buf2, 0, 512 * 8)
+                print("try to decode buffer contents")
+                buf2_contents = bytes(buf2.view[0:5]).decode("ascii")
+                print(buf2_contents)
+                assert buf2_contents == msg
+                print("free buf2")
+        except:
+            import time
+            time.sleep(2)
+
+        print("close fs")
