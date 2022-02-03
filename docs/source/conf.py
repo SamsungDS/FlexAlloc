d26f2f210295913bad4577dbd3df0ed05c879b30
Jesper Wendel Devantier <j.devantier@samsung.com>
2022-02-03 13:02:52 +0100

    Doc: Add sphinx sphinx requirements
    Signed-off-by: Jesper Wendel Devantier <j.devantier@samsung.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,62 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+from pathlib import Path
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'FlexAlloc'
+copyright = '2022, Samsung'
+author = 'Adam Manzanares, Jesper Wendel Devantier, Joel Andres Granados'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'breathe'
+]
+
+# '/path/to/flexalloc/docs/source/conf.py' -> '/path/to/flexalloc/docs'
+docs_root = Path(__file__).resolve().parent.parent
+
+breathe_projects = {
+    "flexalloc": str(docs_root / "doxygen.build" / "xml")
+}
+breathe_default_project = "flexalloc"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
