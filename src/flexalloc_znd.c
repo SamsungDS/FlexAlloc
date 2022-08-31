572fbaf4f2e8c719f3cc62b430ede66555bdf024
Joel Granados <j.granados@samsung.com>
2022-08-31 10:44:17 +0200

    ZNS: Apply commands to all objs in strp objs
    If we encounter a failure while sending a command to one of the objects in
    a striped object, it would fail and ignore the rest of the striped object.
    Instead we try to send the command down to all the objects, keep the error
    value and return it at the end of the functions
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- src/flexalloc_znd.c
+++ src/flexalloc_znd.c
@@ -14,11 +14,10 @@ fla_znd_manage_zones_object_finish(struct flexalloc *fs, struct fla_pool const *
 
   for (uint32_t strp = 0; strp < pool_entry->strp_nobjs; strp++)
   {
-    err = fla_xne_dev_znd_send_mgmt(fs->dev.dev, obj_slba + (fs->geo.nzsect * strp),
-                                    XNVME_SPEC_ZND_CMD_MGMT_SEND_FINISH, false);
-    if (FLA_ERR(err, "fla_xne_dev_znd_send_mgmt_finish()"))
-      return err;
+    err |= fla_xne_dev_znd_send_mgmt(fs->dev.dev, obj_slba + (fs->geo.nzsect * strp),
+                                     XNVME_SPEC_ZND_CMD_MGMT_SEND_FINISH, false);
   }
+  FLA_ERR(err, "fla_xne_dev_znd_send_mgmt_finish()");
   return err;
 }
 
@@ -32,12 +31,10 @@ fla_znd_manage_zones_object_reset(struct flexalloc *fs, struct fla_pool const *p
 
   for (uint32_t strp = 0; strp < pool_entry->strp_nobjs; strp++)
   {
-
-    err = fla_xne_dev_znd_send_mgmt(fs->dev.dev, obj_slba + (fs->geo.nzsect * strp),
-                                    XNVME_SPEC_ZND_CMD_MGMT_SEND_RESET, false);
-    if (FLA_ERR(err, "fla_xne_dev_znd_send_mgmt_reset()"))
-      return err;
+    err |= fla_xne_dev_znd_send_mgmt(fs->dev.dev, obj_slba + (fs->geo.nzsect * strp),
+                                     XNVME_SPEC_ZND_CMD_MGMT_SEND_RESET, false);
   }
+  FLA_ERR(err, "fla_xne_dev_znd_send_mgmt_reset()");
   return err;
 }
 
